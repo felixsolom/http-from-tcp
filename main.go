57fd95f4e5e5667ffebfbcd8c30faa7f2b9e38d7
felixsolom <felix.solom@gmail.com>
2025-09-06 12:15:12 +0300

    small reader

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("./messages.txt")
+	if err != nil {
+		log.Fatalf("Unable to open file: %v", err)
+	}
+	defer file.Close()
+
+	for {
+		buff := make([]byte, 8)
+		n, err := file.Read(buff)
+		if err == io.EOF {
+			log.Printf("End of file reached: %v", err)
+			break
+		}
+		if err != nil {
+			log.Fatalf("unable to read from file: %v", err)
+		}
+
+		snippet := string(buff[:n])
+		fmt.Printf("read: %s\n", snippet)
+	}
+	fmt.Println("Successfully finished reading the file")
 }
