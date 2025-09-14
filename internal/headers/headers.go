7ee29b4115d7e768eb4aabe3256a0d9b54cffd87
felixsolom <felix.solom@gmail.com>
2025-09-14 23:57:28 +0300

    headers#2

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -1,1 +1,35 @@
 package headers
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+type Headers map[string]string
+
+func NewHeaders() Headers {
+	return make(Headers)
+}
+
+const crlf = "\r\n"
+
+func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	idx := bytes.Index(data, []byte(crlf))
+	if idx == -1 {
+		return 0, false, nil
+	}
+	if idx == 0 {
+		return 0, true, nil
+	}
+	newHeader := string(data[:idx])
+	colonIdx := strings.Index(newHeader, ":")
+	if colonIdx == 0 || colonIdx == -1 || newHeader[colonIdx-1] == ' ' {
+		return 0, false, fmt.Errorf("Malformed header")
+	}
+	fieldLine := strings.Split(newHeader, string(newHeader[colonIdx+1]))
+	fieldName := strings.TrimSpace(fieldLine[0])
+	fieldValue := strings.TrimSpace(fieldLine[1])
+	h[fieldName] = fieldValue
+	return idx + len(crlf), false, nil
+}
