6ef56b2f781af04805f838b1f9c629393842eaaf
felixsolom <felix.solom@gmail.com>
2025-09-19 18:24:33 +0300

    server#3

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,24 +2,26 @@ package server
 
 import (
 	"fmt"
+	"log"
 	"net"
 )
 
 type Server struct {
-	State    string
 	Listener net.Listener
+	State    bool
 }
 
 func Serve(port int) (*Server, error) {
-	portStr := fmt.Sprint(port)
+	portStr := fmt.Sprintf(":%d", port)
 	l, err := net.Listen("tcp", portStr)
 	if err != nil {
 		return nil, fmt.Errorf("Coudln't start server on port: %d", port)
 	}
 	server := Server{
 		Listener: l,
-		State:    "initialized",
+		State:    true,
 	}
+	go server.listen()
 	return &server, nil
 }
 
@@ -28,6 +30,35 @@ func (s *Server) Close() error {
 	if err != nil {
 		return err
 	}
-	s.State = "closed"
+	s.State = false
 	return nil
+}
+
+func (s *Server) listen() {
+	for s.State {
+		// Wait for a connection.
+		conn, err := s.Listener.Accept()
+		if err != nil {
+			if s.State == false {
+				break
+			}
+			continue
+		}
+		log.Println("Accept error:", err)
+		s.handle(conn)
+	}
+}
+
+func (s *Server) handle(conn net.Conn) {
+	go func(c net.Conn) {
+		c.Write([]byte(
+			"HTTP/1.1 200 OK\r\n" +
+				"Content-Type: text/plain\r\n" +
+				"Content-Length: 13\r\n" +
+				"\r\n" +
+				"Hello World!\n",
+		))
+		// Shut down the connection.
+		c.Close()
+	}(conn)
 }
