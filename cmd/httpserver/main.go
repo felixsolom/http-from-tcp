21f569caf32f3d23076e63bd84bce144864f90c4
felixsolom <felix.solom@gmail.com>
2025-09-24 15:11:13 +0300

    handler-state-machine

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io"
 	"log"
 	"os"
 	"os/signal"
@@ -28,19 +27,51 @@ func main() {
 	log.Println("Server gracefully stopped")
 }
 
-func handler(w io.Writer, req *request.Request) *server.HandlerError {
+func handler(w *response.Writer, req *request.Request) *server.HandlerError {
 	if req.RequestLine.RequestTarget == "/yourproblem" {
 		return &server.HandlerError{
 			StatusCode: response.BadRequest,
-			Message:    "Your problem is not my problem\n",
+			Message: `
+			<html>
+				<head>
+					<title>400 Bad Request</title>
+				</head>
+				<body>
+					<h1>Bad Request</h1>
+					<p>Your request honestly kinda sucked.</p>
+				</body>
+				</html>`,
 		}
 	}
 	if req.RequestLine.RequestTarget == "/myproblem" {
 		return &server.HandlerError{
 			StatusCode: response.InternalServerError,
-			Message:    "Woopsie, my bad\n",
+			Message: `
+			<html>
+				<head>
+					<title>500 Internal Server Error</title>
+				</head>
+				<body>
+					<h1>Internal Server Error</h1>
+					<p>Okay, you know what? This one is on me.</p>
+				</body>
+				</html>`,
 		}
 	}
-	w.Write([]byte("All good, frfr\n"))
+	w.WriteStatusLine(response.OK)
+	body := []byte(
+		`
+		<html>
+			<head>
+				<title>200 OK</title>
+			</head>
+			<body>
+				<h1>Success!</h1>
+				<p>Your request was an absolute banger.</p>
+			</body>
+			</html>`,
+	)
+	w.WriteHeaders(response.GetDefaultHeaders(len(body), "text/html"))
+	w.WriteBody(body)
 	return nil
 }
